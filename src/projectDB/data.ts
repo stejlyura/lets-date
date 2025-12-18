d63290f4d45d3a84886d9414b98477ddf34913b4
Yurii <stejlyura19@gmail.com>
2025-12-18 08:26:15 +0200

    fix design

--- src/projectDB/data.ts
+++ src/projectDB/data.ts
@@ -1,26 +1,9 @@
 import type { userData } from "../types/type-user";
 
-import Alla from "@/assets/img/Alla.jpg";
-import Anfa from "@/assets/img/Anfa.jpg";
-import Angel from "@/assets/img/Angel.jpg";
-import Anna from "@/assets/img/Anna.webp";
-import Dana from "@/assets/img/Dana.jpg";
-import Eliza from "@/assets/img/Eliza.webp";
-import Karina from "@/assets/img/Karina.jpg";
-import Nastya from "@/assets/img/Nastya.jpg";
-import Nora from "@/assets/img/Nora.jpg";
-import Rara from "@/assets/img/Rara.jpg";
+import Anna from '@/assets/img/Anna.webp';
+
 
 export const projectBD: userData[] = [
-    {data_id: 1, fname: 'Alla', age: 23, isLike: true, mainImg: Alla, isActive: true, range: 20},
-    {data_id: 2, fname: 'Anfa', age: 25, isLike: false, mainImg: Anfa, isActive: false, range: 35},
-    {data_id: 3, fname: 'Angel', age: 22, isLike: true, mainImg: Angel, isActive: true, range: 15},
-    {data_id: 4, fname: 'Anna', age: 24, isLike: false, mainImg: Anna, isActive: false, range: 50},
-    {data_id: 5, fname: 'Dana', age: 26, isLike: true, mainImg: Dana, isActive: true, range: 10},
-    {data_id: 6, fname: 'Eliza', age: 23, isLike: false, mainImg: Eliza, isActive: false, range: 40},
-    {data_id: 7, fname: 'Karina', age: 27, isLike: true, mainImg: Karina, isActive: true, range: 5},
-    {data_id: 8, fname: 'Nastya', age: 21, isLike: false, mainImg: Nastya, isActive: false, range: 30},
-    {data_id: 9, fname: 'Nora', age: 24, isLike: true, mainImg: Nora, isActive: true, range: 12},
-    {data_id: 10, fname: 'Rara', age: 22, isLike: false, mainImg: Rara, isActive: false, range: 45}
+    {data_id: 1, fname: 'Anna', age: 24, isLike: false, mainImg: Anna, isActive: false, range: 50},
 ]
 //    {data_id: 0, fname: '//////', age: 20, isLike: true, mainImg: ////, isActive: true, range: 20},
